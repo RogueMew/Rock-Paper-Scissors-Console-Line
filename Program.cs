5a8268883a5a32e2873bcf2fad36d2044636fb0c
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-12-02 22:29:41 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Rock, Paper, or Scissors");
+
+
+        string userChoice = Console.ReadLine();
+
+
+        if (userChoice == null)
+        {
+            throw new Exception("Input cannot be ");
+        }
+
+        if (userChoice == "")
+        {
+            Console.WriteLine("Hello")
+        }
+
+        Console.WriteLine(userChoice.Length);
+    }
+}
