0cc51e97770d7d8cef96a7be389d0953e3096eb4
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-12-02 23:35:18 -0800

    Base Functions Implemented

--- Program.cs
+++ Program.cs
@@ -1,23 +1,51 @@
 ﻿internal class Program
 {
+    static string CharToString(string userInput)
+    {
+        Dictionary<char, string> choices =
+                new Dictionary<char, string>(){
+                    {'r', "Rock" },
+                    { 'p', "Paper"},
+                    { 's', "Scissors"}
+                };
+        
+        foreach (KeyValuePair<char, string> temp in choices)
+        {
+            if (char.Parse(userInput) == temp.Key)
+            {
+                userInput = temp.Value;
+                break;
+            }
+        }
+
+        if (userInput.Length == 1)
+        {
+            throw new Exception($"{userInput} is not a choice");
+        }
+        
+        return userInput;
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Rock, Paper, or Scissors");
+        Console.WriteLine("Rock, Paper, or Scissors?");
 
-
-        string userChoice = Console.ReadLine();
-
+        string ?userChoice = Console.ReadLine();
 
         if (userChoice == null)
         {
-            throw new Exception("Input cannot be ");
+            throw new Exception($"Input cannot be {userChoice}");
         }
 
         if (userChoice == "")
         {
-            Console.WriteLine("Hello")
+            throw new Exception("Input Not Detected Try Again");
         }
 
-        Console.WriteLine(userChoice.Length);
+        if (userChoice.Length == 1)
+        {
+            userChoice = CharToString(userChoice);
+        }
+
+        
     }
 }
